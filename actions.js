7ccf70b763028dbc94eb7c7d5f4a2dfacf2ee844
Rob Salzberg <robsalzberg@gmail.com>
2018-12-08 13:12:28 -0500

    Added action routes and endpoints

---
+++ actions.js
@@ -0,0 +1,34 @@
+const actions = require('express').Router();
+const actionModel = require('./data/helpers/actionModel')
+
+actions.get('/', (req,res) => {
+    actionModel.get()
+    .then(response => res.send(response))
+    .error(err => res.send(err))
+})
+
+actions.get('/:id', (req,res) => {
+    actionModel.get(req.params.id)
+    .then(response => res.send(response))
+    .error(err => res.send(err))
+})
+
+actions.post('/',(req,res) => {
+    actionModel.insert(req.body)
+    .then(response => res.send(response))
+    .error(err => res.send(err))
+})
+
+actions.put('/:id', (req,res) => {
+    actionModel.update(req.params.id,req.body)
+    .then(response => res.send(response))
+    .error(err => res.sendStatus(500))
+})
+
+actions.delete('/:id', (req,res) => {
+    actionModel.remove(req.params.id)
+    .then(response => res.sendStatus(200))
+    .error(err => res.sendStatus(500))
+})
+
+module.exports = actions;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
-
+const action = require('./actions')
 const app = express()
 
 const port = 4100;
+
+app.get('/', (req,res) => res.send('Hello'))
+app.use('/actions', action);
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
